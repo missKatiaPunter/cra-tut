5adda2c32e17b58ec6f910180fa10426eedec0cf
Katia <katia.punter@storyshareplatform.com>
2020-11-26 20:11:49 +0000

    Sets up home route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div>
-      Hi
-    </div>
+    <Switch>
+      <Route path="/">
+        Hello. This is home page.
+      </Route>
+    </Switch>
   );
 }
 
