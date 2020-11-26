e9a534242c0c8ec5210a34e7ca2a5c6d4c1d2a50
Katia <katia.punter@storyshareplatform.com>
2020-11-26 21:01:59 +0000

    Adds navbar and creates nav components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React from 'react';
+import Navs from './components/Navs';
+import Home from './pages/Home';
+import Starred from './pages/Starred';
 import { Switch, Route } from 'react-router-dom';
 
 const App = () => {
   return (
-    <Switch>
-      <Route exact path="/">
-        Hello. This is home page.
-      </Route>
-      <Route exact path="/starred">
-        This is starred page.
-      </Route>
-    </Switch>
+    <div>
+      <Navs />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/starred">
+          <Starred />
+        </Route>
+        <Route>
+          This is 404 page.
+        </Route>
+      </Switch>
+    </div>
   );
 }
 
