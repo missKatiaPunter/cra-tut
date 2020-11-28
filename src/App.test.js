aa22046cd61c060ebec6cc8faf75c6e108c6c946
Katia <katia.punter@storyshareplatform.com>
2020-11-28 09:07:37 +0000

    Passing test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { MemoryRouter } from 'react-router-dom';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders the router links', () => {
+  render(<App />, { wrapper: MemoryRouter });
+  expect(screen.getByText(/Starred/i)).toBeInTheDocument();
 });
+
