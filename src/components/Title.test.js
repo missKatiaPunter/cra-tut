91bdd2263613abbfec869751bcd80e026d7bda1b
Katia <katia.punter@storyshareplatform.com>
2020-11-29 21:06:06 +0000

    Adds search input

--- src/components/Title.test.js
+++ src/components/Title.test.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import Title from './Title';
-import { getByRole, render } from '@testing-library/react';
+import { render } from '@testing-library/react';
 
 describe('renders title as expected', () => {
   it('should find div element', () => {
     const FIXTURE_PROPS = { title: 'Hello', subtitle: 'world' };
-    const { getByText, getAllByRole } = render(<Title {...FIXTURE_PROPS} />);
+    const { getByText } = render(<Title {...FIXTURE_PROPS} />);
     expect(getByText(/hello/i)).toBeDefined();
     expect(getByText(/world/)).toBeDefined();
   });
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MainPageLayout from '../components/MainPageLayout';
 
 const Home = () => {
+
+    const [searchInputBoxValue, setSearchInputBoxValue] = useState('');
+
+    const handleSearchChange = ( { target: { value }}) => {
+        setSearchInputBoxValue(value);
+    };
+
     return(
         <MainPageLayout>
-            Home
+            <label htmlFor="search-input">Search</label>
+            <input
+                id="search-input"
+                type="text"
+                placeholder="Enter search term"
+                onChange={handleSearchChange}
+            />
         </MainPageLayout>
-
     )
 };
 
