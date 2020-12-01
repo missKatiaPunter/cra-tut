10fe5f2d6853c411fede475c29e5ddb718394e14
Katia <katia.punter@storyshareplatform.com>
2020-12-01 17:47:47 +0000

    Adds tests to Home

--- src/pages/Home.test.js
+++ src/pages/Home.test.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import Home from './Home';
 
 describe('renders home as expected', () => {
     it('should find label and button element', () => {
-      render(<Home />);
+        render(<Home />, { wrapper: MemoryRouter });
       expect(screen.getByRole('button')).toBeDefined();
-    //   expect(getByText(/world/)).toBeDefined();
+      const label = screen.getByLabelText('Search')
+      expect(label).toBeDefined();
     });
   });
 
